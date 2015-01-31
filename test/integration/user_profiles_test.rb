aeb0febcc923d71c8a0f9a794ac7b13498abafb2
Kirk Okada <kirkokada@gmail.com>
2015-01-30 14:09:18 -1000

    Update styling

--- test/integration/user_profiles_test.rb
+++ test/integration/user_profiles_test.rb
@@ -5,11 +5,18 @@ class UserProfilesTest < ActionDispatch::IntegrationTest
 		@user = users(:michael)
 	end
 
-	test "profile page should show user's submitted links" do
+	test "profile page should show user's submitted links with delete links" do
+		sign_in @user
 		get user_path(@user)
 		@user.links.paginate(page: 1).each do |link|
 			assert_match link.title, response.body
 			assert_match CGI.escapeHTML(link.url), response.body
+			assert_select 'a[href=?]', link_path(link), text: 'delete'
+		end
+		sign_out
+		get user_path(@user)
+		@user.links.paginate(page: 1).each do |link|
+			assert_select 'a[href=?]', link_path(link), text: 'delete', count: 0
 		end
 	end
 end
